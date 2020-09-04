3698e076d979b247b6d79a5de05f128533625fd6
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-09-04 22:51:38 +1000

    Improve my_statuslines.bash formatting of panes

--- conf.d/my_statuslines.bash
+++ conf.d/my_statuslines.bash
@@ -1,3 +1,4 @@
+source ~/.tmux/style.conf
 
 _decode_unicode_escapes() {
   printf '%s' "$*" | perl -CS -pe 's/(\\u([0-9A-Fa-f]{1,4})|\\U([0-9A-Fa-f]{1,8}))/chr(hex($2.$3))/eg' 2>/dev/null
@@ -9,23 +10,36 @@ tmux_conf_theme_left_separator_sub='\uE0B1'   #   you only need fonts patched wi
 tmux_conf_theme_right_separator_main='\uE0B2' #   Powerline symbols or the standalone
 tmux_conf_theme_right_separator_sub='\uE0B3'  #   PowerlineSymbols.otf font, see README.md
 
-_ms="$tmux_conf_left_separator_main"
+_ms="$tmux_conf_theme_left_separator_main"
 _ss="$tmux_conf_theme_left_separator_sub"
+
+
 
 # Status format_1
 # (Line 2) 
 # This will display a list of panes, their dims, in cute colors
 _status_format_1 () {
-  cols_1='#[fg=black#,bg=#00afff]'
-  cols_2="#[bg=black#,fg=#00afff]"
+  col_active="${tmux_conf_theme_pane_active_border}"
+  col_off="${tmux_conf_theme_pane_border}"
 
-  out = "#[align=centre]#{P:#{?pane_active,${ms}${cols_1},$cols_2}#{pane_index}[#{pane_width}x#{pane_height}]}#[default] "
+  active_main="#[fg=black#,bg=$col_active]"
+  active_flip="#[fg=$col_active#,bg=black]"
   
-  out = _decode_unicode_escapes "$out"
-  echo $out
+  off_main="#[fg=black#,bg=$col_off]"
+  off_main="#[reverse]"
+  off_flip="#[fg=$col_off,bg=black]"
+  off_flip="#[default]"
+
+  head_sep="#{?pane_active,${active_main},${off_main}}${_ms} "
+  tail_sep="#{?pane_active,${active_flip},${off_flip}}${_ms} "
+  out="#[align=centre]#{P:#[default]${head_sep}#{pane_index}${_ss} #{pane_width}x#{pane_height}${tail_sep}#[default]}"
+  
+  echo "$out"
 }
 
-tmux set -g status-format[1] "$(_status_format_1)"
+
+tmux set -g status-format[1] "$(_decode_unicode_escapes "$(_status_format_1)")"
+
 
 # status_left=$(echo "$status_left" | sed 's%#{circled_session_name}%#(cut -c3- ~/.tmux/apply.conf | sh -s _circled #S)%g')
 
